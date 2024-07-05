7cc2cf7d82438a49b7b18a77f5a975cc523052fd
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-07-05 21:10:18 +0300

    detect disconnection as error

--- cmd/error.sh
+++ cmd/error.sh
@@ -1,9 +1,16 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -i dash --pure --keep CREDENTIALS_DIRECTORY --keep XDG_RUNTIME_DIR -I channel:nixos-23.11-small -p curl cacert flock findutils dash jq 
 
-val="$(dash ./data.sh | jq -r '.attributes.mower.state')"
-if [ "$val" != "IN_OPERATION" ] && [ "$val" != "OFF" ]; then
-    echo 1
-else
-    echo 0
-fi
+data="$(dash ./data.sh)"
+echo "$data"\
+  | jq -r '[.attributes.mower.state, .attributes.metadata.connected] | @tsv'\
+  | { 
+      read -r state connected
+      if [ "$state" != "IN_OPERATION" ] && [ "$state" != "OFF" ]; then
+          echo 1
+      elif [ "$connected" = "false" ]; then
+          echo 1
+      else
+          echo 0
+      fi
+  }
